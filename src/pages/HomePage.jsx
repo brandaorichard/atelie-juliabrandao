6f0dc87ae912cc56ae3694fe268d889821d2e7c1
Ricardo Brandão <126647257+brandaorichard@users.noreply.github.com>
2025-08-24 15:21:16 -0300

    feat: add Category3Preview component to HomePage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,7 @@
 import Hero from "../components/Hero";
 import Category1Preview from "../components/Category1Preview";
 import Category2Preview from "../components/Category2Preview";
+import Category3Preview from "../components/Category3Preview";
 
 export default function HomePage() {
   return (
@@ -8,6 +9,7 @@ export default function HomePage() {
       <Hero />
       <Category1Preview />
       <Category2Preview />
+      <Category3Preview />
     </div>
   );
 }
